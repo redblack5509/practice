8049a96de88b7d60ab9d5a0202c76857dca65cbc
leon <liuliangming.foxmail.com>
2017-06-29 00:04:50 +0800

    代码

---
+++ bing_wallpaper/crontab_set.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 添加到crontab，一个小时执行一次
+# 0 */1 * * * /home/leon/github/practice/bing_wallpaper/crontab_set.sh
+
+# 判断今天是否已经更新了壁纸
+date=`date +"%Y%m%d"`
+TMP_FILE="/tmp/check_wallpaper_${date}"
+if [ -f ${TMP_FILE} ];then
+    exit 0
+fi
+
+# export DBUS_SESSION_BUS_ADDRESS environment variable
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+# 设置壁纸
+/home/leon/github/practice/bing_wallpaper/set_bing_wallpaper.py
+
+# 标记今天已经更新了壁纸
+touch ${TMP_FILE}
---
+++ bing_wallpaper/set_bing_wallpaper.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+# ubuntu设置动态bing壁纸
+
+import requests
+import os, re
+
+save_dir = "/home/leon/pic/wallpaper/bing/"
+
+def set_desktop(pic_path):
+    cmd = "gsettings set org.gnome.desktop.background picture-uri file:" + pic_path
+    # 设置壁纸
+    os.system(cmd)          
+
+    
+#获取今天的图片的url和copyright信息
+def get_bing_info():
+    base_url = "http://cn.bing.com/"
+    query_url = "HPImageArchive.aspx?format=js&idx=0&n=1"
+
+    r = requests.get(base_url + query_url)
+
+    image_url = base_url + r.json()["images"][0]["url"]
+    copyright = r.json()["images"][0]["copyright"]
+    copyright = re.sub(r'\(.*\)', '', copyright);
+    print(copyright)
+
+    return (image_url, copyright)
+
+#下载图片
+def down_pic(pic_url, save_dir):
+    image_name = re.sub(r'.*/', '', pic_url)
+    save_path = save_dir + image_name
+    print(save_path)
+
+    r = requests.get(pic_url)
+    file = open(save_path, "wb")
+    file.write(r.content)
+    file.close()
+
+    return save_path
+
+#添加水印
+def add_watermark(path, text):
+    new_path = path + ".watermark"
+    cmd = "convert {0} -font 文泉驿微米黑 -pointsize 30 -draw \"gravity south fill black  text 0,72 '{1}'  fill white  text 1,71 '{1}' \" {2}".format(path, text, new_path)
+
+    #print(cmd)
+    os.system(cmd)   
+    os.system("mv {} {}".format(new_path, path))
+
+def main():
+    url, copyright = get_bing_info()
+    path = down_pic(url, save_dir)
+    add_watermark(path, copyright)
+    set_desktop(path)
+
+main()
